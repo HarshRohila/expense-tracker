c8edd709ad952725b98661123873a864354d5202
Harsh Rohila <rohilaharsh@gmail.com>
2023-12-08 23:57:26 +0530

    testing bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 import { safeInnerHtml } from './utils/index.ts'
 // Import our custom CSS
 import './scss/styles.scss'
@@ -9,7 +8,7 @@ import './scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 const html = `
-<div>
+<div class="container">
   <button class="btn btn-primary">Primary button</button>
   <a href="https://vitejs.dev" target="_blank">
     <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -17,13 +16,6 @@ const html = `
   <a href="https://www.typescriptlang.org/" target="_blank">
     <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
   </a>
-  <h1>Vite + TypeScript</h1>
-  <div class="card">
-    <button id="counter" type="button"></button>
-  </div>
-  <p class="read-the-docs">
-    Click on the Vite and TypeScript logos to learn more
-  </p>
 </div>
 `
 
@@ -31,4 +23,25 @@ const appEl = document.querySelector<HTMLDivElement>('#app')
 
 safeInnerHtml(appEl!, html)
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let testForm = `
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+`
+
+let containerEl = document.querySelector(".container")
+
+safeInnerHtml(containerEl!, testForm)
